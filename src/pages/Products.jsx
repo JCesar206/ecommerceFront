5a0f8a2078369f14e3ce03881bb034fa2b62c386
Julio Cesar <jcesar206@hotmail.com>
2026-07-13 07:49:09 -0600

    Se agrega componente Product a la app

---
+++ src/pages/Products.jsx
@@ -0,0 +1,11 @@
+function Products() {
+	return (
+		<div className="min-h-screen flex items-center justify-center">
+			<h1 className="text-4xl font-bold">
+				Productos
+			</h1>
+		</div>
+	);
+}
+
+export default Products;
