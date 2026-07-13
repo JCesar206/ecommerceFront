a58814cc469714d7bebc8a6aa876e0327c380e2e
Julio Cesar <jcesar206@hotmail.com>
2026-07-13 07:43:56 -0600

    Se agrega componente Footer a la app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+	return (
+		<footer className="bg-gray-900 text-white py-6">
+			<div className="text-center">
+				<p>
+					&lt;copy&rt; {new Date().getFullYear()} Ecommerce FullStack
+				</p>
+			</div>
+		</footer>
+	);
+}
+
+export default Footer;
