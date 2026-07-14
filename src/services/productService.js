69625a50f84f54964709c92a8e7bb1aef4f63c37
Julio Cesar <jcesar206@hotmail.com>
2026-07-14 10:13:49 -0600

    Se actualiza la lógica del archivo para el buen manejo de los datos en el crud

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,21 +1,23 @@
 import api from "../api/axios.js";
-const productService = {
-	getAll: async () => {
-		const response = await api.get("/products");
-		return response.data;
-	},
-	getById: async (id) => {
-		const response = await api.get(`/products${id}`);
-		return response.data;
-	},
-	update: async (id, productData) => {
-		const response = await api.put(`/products/${id}`, productData);
-		return response.data;
-	},
-	remove: async (id) => {
-		const response = await api.delete(`/products/${id}`);
-		return response.data;
-	}
+const getProducts = async () => {
+	const response = await api.get("/products");
+	return response.data;
+};
+const getProductById = async (id) => {
+	const response = await api.get(`/products/${id}`);
+	return response.data;
+};
+const createProduct = async (FormData) => {
+	const response = await api.post("/products", FormData, {headers: { "Content-Type": "multipart/form-data"}});
+	return response.data;
+}
+const updateProduct = async (id, FormData) => {
+	const response = await api.put(`/products${id}`, FormData, {headers: { "Content-Type": "multipart/form-data"}});
+	return response.data;
+};
+const deleteProduct = async (id) => {
+	const response = await api.delete(`/products${id}`);
+	return response.data;
 };
 
-export default productService;
+export default {getProducts, getProductById, createProduct, updateProduct, deleteProduct};
