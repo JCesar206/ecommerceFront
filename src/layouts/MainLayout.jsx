7c7025f598624ef968b1ee7016bce637d3219e29
Julio Cesar <jcesar206@hotmail.com>
2026-07-15 09:53:18 -0600

    Se actualiza el estilo del componente de manera global

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar.jsx";
-import Footer from "../components/Footer.jsx";
+import Navbar from "../components/layout/Navbar.jsx";
+import Footer from "../components/layout/Footer.jsx";
 
 function MainLayout() {
 	return (
-		<div className="min-h-screen flex flex-col">
+		<div className="min-h-screen flex flex-col font-semibold bg-gray-50 text-gray-900 dark:bg-gray-950 dark:text-gray-100 transition-colors duration-300">
 			<Navbar/>
 			<main className="flex-1">
 				<Outlet/>
