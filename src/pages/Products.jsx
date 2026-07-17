3662c075e0a4ae3f01e1834987a09d867ece416f
Julio Cesar <jcesar206@hotmail.com>
2026-07-17 17:25:11 -0600

    Se actualiza la lógica del archivo

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,47 @@
+import { useEffect, useMemo, useState } from "react";
+import productService from "../services/productService.js";
+import ProductCard from "../components/product/ProductCard.jsx";
+import ProductGrid from "../components/product/ProductGrid.jsx";
+import SearchBar from "../components/product/SearchBar.jsx";
+
 function Products() {
+  const [products, setProducts] = useState([]);
+  const [search, setSearch] = useState("");
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const loadProducts = async () => {
+			try {
+				const response = await productService.getProducts();
+				setProducts(response.data);
+			} catch (error) {
+				console.error(error);
+			} finally {
+				setLoading(false);
+			}
+		};
+		loadProducts();
+	}, []);
+	const filteredProducts = useMemo(() => {
+		return products.filter(product => 
+			product.name.toLowerCase().includes(search.toLowerCase())
+		);
+	}, [products, search]);
+	if (loading) {
+		return (
+			<div className="text-center mt-20 text-xl">Cargando productos...</div>
+		);
+	}
+
 	return (
-		<div className="min-h-screen flex items-center justify-center">
-			<h1 className="text-4xl font-bold">
-				Productos
-			</h1>
+		<div className="max-w-7xl mx-auto px-4 py-8">
+			<h1 className="text-4xl font-bold mb-6">Productos</h1>
+			<SearchBar value={search} onChange={(e)=>setSearch(e.target.value)}/>
+				<ProductGrid>
+					{filteredProducts.map(product => (
+						<ProductCard key={product.id} product={product}/>
+					))}
+				</ProductGrid>
 		</div>
 	);
 }
