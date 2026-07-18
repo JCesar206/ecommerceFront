141223c503a17d2bc3deada1939cb6d1ca171af9
Julio Cesar <jcesar206@hotmail.com>
2026-07-17 18:01:00 -0600

    Se actualiza la lógica del archivo

--- src/services/productService.js
+++ src/services/productService.js
@@ -7,16 +7,16 @@ const getProductById = async (id) => {
 	const response = await api.get(`/products/${id}`);
 	return response.data;
 };
-const createProduct = async (FormData) => {
-	const response = await api.post("/products", FormData, {headers: { "Content-Type": "multipart/form-data"}});
+const createProduct = async (formData) => {
+	const response = await api.post("/products", formData, {headers: { "Content-Type": "multipart/form-data"}});
 	return response.data;
 }
-const updateProduct = async (id, FormData) => {
-	const response = await api.put(`/products${id}`, FormData, {headers: { "Content-Type": "multipart/form-data"}});
+const updateProduct = async (id, formData) => {
+	const response = await api.put(`/products/${id}`, formData, {headers: { "Content-Type": "multipart/form-data"}});
 	return response.data;
 };
 const deleteProduct = async (id) => {
-	const response = await api.delete(`/products${id}`);
+	const response = await api.delete(`/products/${id}`);
 	return response.data;
 };
 
