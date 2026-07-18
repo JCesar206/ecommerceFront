b3693da5921e48c88f8252632eb717350b433cd6
Julio Cesar <jcesar206@hotmail.com>
2026-07-18 07:16:13 -0600

    Se actualiza la lógica y diseño del componente

--- src/pages/admin/AdminProducts.jsx
+++ src/pages/admin/AdminProducts.jsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
 import productService from "../../services/productService.js";
 import formatCurrency from "../../utils/formatCurrency.js";
-import ProductForm from "./ProductForm.jsx"
+import ProductForm from "./ProductForm.jsx";
+import { useLanguage } from "../../context/LanguageContext.jsx";
 
 function AdminProducts() {
 	const [products, setProducts] = useState([]);
 	const [loading, setloading] = useState(true);
 	const [showForm, setShowForm] = useState(false);
 	const [selectedProduct, setSelectedProduct] = useState(null);
+	const { language, toggleLanguage, t} = useLanguage();
 
 	const loadProducts = async () => {
 		try {
@@ -57,18 +59,17 @@ function AdminProducts() {
 
 	if (loading) {
 		return (
-			<div className="text-center py-10">Cargando productos...</div>
+			<div className="text-center py-10">{t("admin","loadingProducts")}</div>
 		);
 	}
-console.log("ESTOY EN ADMIN PRODUCTS", products);
 
 	return (
 		<div className="max-w-7xl mx-auto py-10 px-4">
 			<div className="flex justify-between items-center mb-8">
 				{showForm && (<ProductForm product={selectedProduct} onSuccess={handleSuccess} onCancel={handleCancel} />)}
 
-				<h1 className="text-3xl font-bold">Administración de productos</h1>
-				<button onClick={handleCreate} className="bg-green-600 hover:bg-green-800 text-white font-semibold px-5 py-2 rounded cursor-pointer">Producto nuevo</button>
+				<h1 className="text-3xl font-bold">{t("admin","productManagment")}</h1>
+				<button onClick={handleCreate} className="bg-green-600 hover:bg-green-800 text-white font-semibold px-5 py-2 rounded cursor-pointer">{t("admin","newProduct")}</button>
 			</div>
 			<div className="grid md:grid-cols-3 gap-6">
 				{products.map(product => (
@@ -83,8 +84,8 @@ console.log("ESTOY EN ADMIN PRODUCTS", products);
 						<p className="font-bold mt-3">{formatCurrency(product.price)}</p>
 						<p>Stock: {product.stock}</p>
 						<div className="flex flex-col gap-3 mt-5">
-							<button onClick={() => handleEdit(product)} className="bg-blue-600 hover:bg-blue-800 text-white font-semibold px-4 py-2 rounded cursor-pointer">Editar</button>
-							<button onClick={() => handleDelete(product.id)} className="bg-red-600 hover:bg-red-800 text-white font-semibold px-4 py-2 rounded cursor-pointer">Eliminar</button>
+							<button onClick={() => handleEdit(product)} className="bg-blue-600 hover:bg-blue-800 text-white font-semibold px-4 py-2 rounded cursor-pointer">{t("admin","edit")}</button>
+							<button onClick={() => handleDelete(product.id)} className="bg-red-600 hover:bg-red-800 text-white font-semibold px-4 py-2 rounded cursor-pointer">{t("admin","delete")}</button>
 						</div>
 					</div>
 				))}
