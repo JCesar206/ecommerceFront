1bf8f0c8a4a7deeb59484f1b33c8207faf322bc2
Julio Cesar <jcesar206@hotmail.com>
2026-07-18 07:17:22 -0600

    Se actualiza el diccionario de la app

--- src/translations/texts.js
+++ src/translations/texts.js
@@ -1,5 +1,9 @@
 const texts = {
 	es: {
+		home: {
+			title: "Proyecto desarrollado con React, Tailwind CSS, Node.js, Express, MySQL y JWT Refresh Token.",
+			viewProducts: "Ver productos"
+		},
 		navbar: {
 			home: "Inicio",
 			products: "Productos",
@@ -8,7 +12,35 @@ const texts = {
 			cart: "Carrito",
 			logout: "Cerrar sesión",
 			admin: "Admin Productos",
-			welcome: "Hola"
+			welcome: "Hola",
+			theme: "Tema",
+			language: "Idioma"
+		},
+		login: {
+			title: "Iniciar sesión",
+			email: "Correo electrónico",
+			password: "Contraseña",
+			login: "Iniciar sesión",
+			loggingIn: "Iniciando sesión..."
+		},
+		products: {
+			title: "Productos",
+			addToCart: "Agregar al carrito",
+			loadingProducts: "Cargando productos..."
+		},
+		search: {
+			searchProducts: "Buscar productos..."
+		},
+		admin: {
+			title: "Productos",
+			adminPanel: "Panel administrados",
+			newProduct: "Producto nuevo",
+			createProduct: "Crear producto",
+			users: "Usuarios",
+			managerUser: "Administrar usuario",
+			productManager: "Administrador de productos",
+			edit: "Editar",
+			delete: "Eliminar"
 		},
 		footer: {
 			title: "DevShop Comercio Electrónico FullStack",
@@ -16,15 +48,47 @@ const texts = {
 		}
 	},
 	en: {
+		home: {
+			title: "Project developed using React, Tailwind CSS, Node.js, Express, MySQL, and JWT Refresh Token.",
+			viewProducts: "View products"
+		},
 		navbar: {
 			home: "Home",
 			products: "Products",
 			login: "Login",
-			register: "Registro",
+			register: "Register",
 			cart: "Cart",
 			logout: "Logout",
 			admin: "Admin products",
-			welcome: "Hello"
+			welcome: "Hello",
+			theme: "Theme",
+			language: "Language"
+		},
+		login: {
+			title: "Login",
+			email: "Email",
+			password: "Password",
+			login: "Login",
+			loggingIn: "Logging in"
+		},
+		products: {
+			title: "Products",
+			addToCart: "Add to cart",
+			loadingProducts: "Loading products..."
+		},
+		search: {
+			searchProducts: "Search products..."
+		},
+		admin: {
+			title: "Products",
+			adminPanel: "Admin panel",
+			newProduct: "New product",
+			createProduct: "Create product",
+			users: "Users",
+			managerUser: "Manager user",
+			productManager: "Product manager",
+			edit: "Edit",
+			delete: "Delete"
 		},
 		footer: {
 			title: "DevShop Ecommerce FullStack",
