6c71b1d9db503980a8c78bf971d3087deac70dfd
Julio Cesar <jcesar206@hotmail.com>
2026-07-19 09:18:27 -0600

    Se sube estructura faltante actualizada del proyecto (Solo app.jsx, index.css, package.json)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
+import AppRouter from "./routes/AppRouter";
+
 function App() {
-  return (
-		<main className="min-h-screen flex items-center justify-center">
-			<h1 className="text-4xl font-bold">
-				Ecommerce FullStack
-			</h1>
-		</main>
-  );
+	return <AppRouter/>;
 }
 
 export default App;
--- src/components/ProductGrid.jsx
+++
@@ -1,9 +0,0 @@
-function ProductGrid({ children }) {
-	return (
-		<section className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-			{children}
-		</section>
-	);
-}
-
-export default ProductGrid;
