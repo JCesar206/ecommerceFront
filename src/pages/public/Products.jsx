be73d0f3f9ffe07fafbb85e90bd077cac4f2c9c0
Julio Cesar <jcesar206@hotmail.com>
2026-07-19 08:50:31 -0600

    Se actualiza la lógica del archivo Products.jsx

--- src/pages/public/Products.jsx
+++ src/pages/public/Products.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 import productService from "../../services/productService.js";
 import ProductGrid from "../../components/product/ProductGrid.jsx";
 import ProductCard from "../../components/product/ProductCard.jsx";
+import { useLanguage } from "../../context/LanguageContext.jsx";
 
 function Products() {
 	const [products, setProducts] = useState([]);
-	const [loading, setLoading] = useState(true);
+	const [loading, loading] = useState(true);
+	const { language, toggleLanguage, t} = useLanguage();
 
 	useEffect(() => {
 		const loadProducts = async () => {
@@ -15,7 +17,7 @@ function Products() {
 			} catch (error) {
 				console.error(error);
 			} finally {
-				setLoading(false);
+				loading(false);
 			}
 		};
 		loadProducts();
@@ -23,14 +25,14 @@ function Products() {
 	if (loading) {
 		return (
 			<div className="text-center mt-20 text-xl">
-				Cargando productos...
+				{t("products","loadingProducts")}
 			</div>
 		);
 	}
 
 	return (
 		<div className="max-w-7xl mx-auto px-4 py-8">
-			<h1 className="text-4xl font-bold mb-8">Productos</h1>
+			<h1 className="text-4xl font-bold mb-8">{t("products","title")}</h1>
 			<ProductGrid>
 				{
 					products.map(product => (
