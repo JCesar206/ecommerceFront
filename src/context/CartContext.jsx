acbdb4d874cb8096bb8afbf524d184b2f6fc32ce
Julio Cesar <jcesar206@hotmail.com>
2026-07-19 08:47:27 -0600

    Se actualiza la estructura del archivo CartContext.jsx

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,7 +1,17 @@
-import { createContext, useContext, useMemo, useState } from "react";
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
+
 const CartContext = createContext();
+
 export function CartProvider({ children }) {
-    const [cartItems, setCartItems] = useState([]);
+    const [cartItems, setCartItems] = useState(() => {
+			const savedCart = localStorage.getItem("cart");
+			return savedCart ? JSON.parse(savedCart) : [];
+		});
+
+	useEffect(() => {
+		localStorage.setItem("cart", JSON.stringify(cartItems));
+	}, [cartItems]);
+
     const addToCart = (product) => {setCartItems(prev => {
       const exists = prev.find(item => item.id === product.id);
         if (exists) {
@@ -11,17 +21,22 @@ export function CartProvider({ children }) {
 
     const removeFromCart = (id) => {
         setCartItems(prev => prev.filter(item => item.id !== id)); };
+
     const increaseQuantity = (id) => {
         setCartItems(prev =>
             prev.map(item => item.id === id ? {...item, quantity: item.quantity + 1} : item));
     		};
+
     const decreaseQuantity = (id) => {
         setCartItems(prev => prev.map(item => item.id === id ? {...item, quantity: item.quantity - 1} : item)
         .filter(item => item.quantity > 0));
     		};
+
     const clearCart = () => {
         setCartItems([]);
+				localStorage.removeItem("cart");
     		};
+				
     const totalItems = useMemo(() => {
         return cartItems.reduce((total, item) => total + item.quantity, 0);
     }, [cartItems]);
