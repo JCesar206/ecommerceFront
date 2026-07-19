c0183a148e3fc2de68d4b7e0489e9f95a60c9c1b
Julio Cesar <jcesar206@hotmail.com>
2026-07-19 08:52:35 -0600

    Se actualiza la lógica del archivo AdminDashboard.jsx

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -1,18 +1,22 @@
+import { useLanguage } from "../../context/LanguageContext.jsx";
+
 function AdminDashboard() {
+	const { language, toggleLanguage, t} = useLanguage();
+
 	return (
 		<div className="max-w-6xl mx-auto py-10">
-			<h1 className="text-3xl font-bold mb-8">Panel Administrador</h1>
+			<h1 className="text-3xl font-bold mb-8"></h1>
 			<div className="grid md:grid-cols-3 gap-6">
-				<h2 className="text-xl font-semibold">Productos</h2>
-				<p className="mt-2 text-gray-600">Administrar catálogo</p>
+				<h2 className="text-xl font-semibold">{t("admin","title")}</h2>
+				<p className="mt-2 text-gray-600">{t("admin","adminPanel")}</p>
 			</div>
 			<div className="border rounded-lg p-6 shadow">
-				<h2 className="text-xl font-semibold">Nuevo producto</h2>
-				<p className="mt-2 text-gray-600">Crear productos</p>
+				<h2 className="text-xl font-semibold">{t("admin","newProduct")}</h2>
+				<p className="mt-2 text-gray-600">{t("admin","createProduct")}</p>
 			</div>
 			<div className="border rounded-lg p-6 shadow">
-				<h2 className="text-xl font-semibold">Usuarios</h2>
-				<p className="mt-2 text-gray-600">Gestionar usuarios</p>
+				<h2 className="text-xl font-semibold">{t("admin","users")}</h2>
+				<p className="mt-2 text-gray-600">{t("admin","managerUser")}</p>
 			</div>
 		</div>
 	);
