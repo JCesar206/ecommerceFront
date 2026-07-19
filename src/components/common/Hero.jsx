49dbfbc2c21b3e889b1ef195c356a19d9a516d04
Julio Cesar <jcesar206@hotmail.com>
2026-07-19 09:07:30 -0600

    Se actualiza lógica y diseño del componente Hero.jsx

--- src/components/common/Hero.jsx
+++ src/components/common/Hero.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
+import { useLanguage } from "../../context/LanguageContext.jsx";
 
 function Hero() {
+	const { language, toggleLanguage, t} = useLanguage();
+
 	return (
 		<section className="bg-slate-900 text-white">
 			<div className="max-w-7xl mx-auto px-6 py-24">
 				<h1 className="text-5xl font-bold leading-tight">Ecommerce FullStack</h1>
-				<p className="mt-6 text-lg text-slate-300 max-w-2xl">Proyecto desarrollado con React, Tailwind CSS, Node.js, Express, MySQL y JWT Refresh Token.</p>
-				<Link to="/products" className="inline-block mt-8 bg-blue-600 hover:bg-blue-700 px-6 py-3 rounded-lg transition">Ver productos</Link>
+				<p className="mt-6 text-lg text-slate-300 max-w-2xl">{t("home","title")}</p>
+				<Link to="/products" className="inline-block mt-8 bg-blue-600 hover:bg-blue-700 px-6 py-3 rounded-lg transition">{t("home","viewProducts")}</Link>
 			</div>
 		</section>
 	);
