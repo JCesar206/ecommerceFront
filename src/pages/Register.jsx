0cb27455e9dc456161942670fe3b0580f7ce4b4b
Julio Cesar <jcesar206@hotmail.com>
2026-07-19 08:46:08 -0600

    Se actualiza la lógica del componente Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useLanguage } from "../../context/LanguageContext.jsx";
+import { useLanguage } from "../context/LanguageContext.jsx";
 import authService from "../services/authService.js";
 
 function Register() {
