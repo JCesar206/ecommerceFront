dde3e92f9d90125e360ec4cabe6b74b27597251d
Julio Cesar <jcesar206@hotmail.com>
2026-07-19 08:44:38 -0600

    Se actualiza la lógica del archivo Checkout.jsx

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,11 +1,14 @@
-import CheckoutFrom from "../components/checkout/CheckoutForm.jsx";
+import CheckoutForm from "../components/checkout/CheckoutForm.jsx";
 import OrderSummary from "../components/checkout/OrderSummary.jsx";
+import { useLanguage } from "../context/LanguageContext.jsx";
 
 function Checkout() {
+	const { t } = useLanguage();
+
 	return (
 		<div className="max-w-7xl mx-auto px-4 py-10">
-			<h1 className="text-4xl font-bold mb-10 dark:text-white">Checkout</h1>
-			<div className="grid lg:grid-cols-3 gap-8">
+			<h1 className="text-4xl font-bold mb-10 dark:text-white">{t("checkout","title")}</h1>
+			<div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
 				<div className="lg:col-span-2">
 					<CheckoutForm />
 				</div>
