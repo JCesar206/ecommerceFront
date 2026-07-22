b1a9a98148e6bd73d85f8da32de6bd4d77057033
Julio Cesar <jcesar206@hotmail.com>
2026-07-22 09:38:19 -0600

    Se actualiza diseño del componente Footer.jsx

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -27,8 +27,9 @@ function Footer() {
 					</div>
 				</div>
 			<div className="text-center">
-				<p className="text-sm text-center py-2 hover:text-blue-500 hover:scale-90 transition">
-					&copy; {new Date().getFullYear()} {t("footer","title")} | <GiAlienBug size={18}/> Juls | {t("footer","rights")}
+				<p className="flex items-center gap-2 text-sm text-center py-2 hover:text-blue-500 hover:scale-90 transition">
+					&copy; {new Date().getFullYear()} <span>{t("footer","title")}</span> | 
+					<GiAlienBug size={18}/> Juls | <span>{t("footer","rights")}</span>
 				</p>
 			</div>
 		</footer>
